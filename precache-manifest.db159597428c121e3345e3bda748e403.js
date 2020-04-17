fe2d6639dcec8a645fa40ac7720c0720a887e099
Shreyash <142shreyash.shreyash@gmail.com>
2020-04-18 04:12:27 +0530

    initial coommit

---
+++ precache-manifest.db159597428c121e3345e3bda748e403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b21b4bb6afc4639a2a044cf1c31696a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a33f61257dbcf578b0b9",
+    "url": "/static/css/main.1838d0f8.chunk.css"
+  },
+  {
+    "revision": "a9711e1aaa65b79faa5d",
+    "url": "/static/js/2.53b15bce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53b15bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a33f61257dbcf578b0b9",
+    "url": "/static/js/main.f08b774a.chunk.js"
+  },
+  {
+    "revision": "5a5f0d95fca730291cbc",
+    "url": "/static/js/runtime-main.8aa5a091.js"
+  }
+]);
